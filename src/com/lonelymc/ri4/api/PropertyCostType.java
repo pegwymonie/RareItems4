a6015aad6b0356503f790031a043eb4e76cc7171
Ne0nx3r0 <Ne0nx3r0@gmail.com>
2015-02-28 10:42:08 -0600

    Intelliderp

--- src/com/lonelymc/ri4/api/PropertyCostType.java
+++ src/com/lonelymc/ri4/api/PropertyCostType.java
@@ -1,5 +1,5 @@
 package com.lonelymc.ri4.api;
 
 public enum PropertyCostType {
-    FOOD, EXPERIENCE, HEALTH, COOLDOWN, AUTOMATIC, PASSIVE, costType;
+    FOOD, EXPERIENCE, HEALTH, COOLDOWN, AUTOMATIC, PASSIVE;
 }
